abf912f13ea85b6d0f93afa499147365b1082b99
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-07-03 16:06:13 +0500

    Implement decorator for addition function
    Added a decorator for the addition function to enhance output.

--- advanced.py
+++ advanced.py
@@ -1,8 +1,21 @@
 # Decorater
-class Animal:
-    @property # ye decorater obj ko call jiye bagheer bhi chla deta ha yani agar hum (obj.show() mian () nhi bhi lgay tu error hi ayga) iski example line numhber 8 pr ha.
-    def show(self):
-        print("hello how are you")
+# class Animal:
+#     @property # ye decorater obj ko call jiye bagheer bhi chla deta ha yani agar hum (obj.show() mian () nhi bhi lgay tu error hi ayga) iski example line numhber 8 pr ha.
+#     def show(self):
+#         print("hello how are you")
 
-obj = Animal()
-obj.show
+# obj = Animal()
+# obj.show
+
+def decorate(func):
+    def wrapper(a,b): #if you don't know how many arguments you re there so you just need to replace (a,b) with (*args)
+        print("The additionof your numbers are")
+        func(a,b)
+        print("Thanks")
+    return wrapper
+
+@decorate
+def addition(a,b):
+    print(f"Your total is {a + b}")
+
+addition(4,6)
