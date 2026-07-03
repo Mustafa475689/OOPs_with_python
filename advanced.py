df7e64e848acd297582963a6fdb298137f1685da
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-07-03 17:17:26 +0500

    Uncomment code examples and explanations in advanced.py
    Uncomment code examples and explanations in advanced.py

--- advanced.py
+++ advanced.py
@@ -21,15 +21,35 @@
 # addition(4,6)
 # ...
 
-# args and kwargs ............. args apke arguments ko tuple form main hty hain
-def addition(*args):
-    sum = 0
-    for i in args:
-        sum = sum + i    
-    print(sum)
+# # args and kwargs ............. args apke arguments ko tuple form main hty hain
+# def addition(*args):
+#     sum = 0
+#     for i in args:
+#         sum = sum + i    
+#     print(sum)
 
-addition(2,3,4,5,6)
+# addition(2,3,4,5,6)
 
-# Kwargs ....................
+# # Kwargs ....................
+# def information(**kwargs):
+#     print("Print your information is \n")
+#     for i in kwargs:
+#         print(f"{i} : {kwargs[i]}")
 
+# information(name = "Mustafa", age = "21", designation = "MERN dev")
+# ...
+
+# List Comphrehension ..................
+# l = [i for i in range(1, 21) if i % 2 == 0]
+# print(l)
+# # ...
+
+# dictionary Comphrehension ..................
+# d = {i : i**2 for i in range(1, 10)}
+# print(d)
+# ...
+
+# set Comphrehension ..................
+# s = {i*i for i in range(1, 21) if i % 2 == 0}
+# print(s)
 # ...
