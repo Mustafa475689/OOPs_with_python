f5e7d5537100b366f8bcebff05dc14c7687cbe46
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-07-03 16:18:16 +0500

    Update addition function to use variable arguments
    Refactored addition function to accept variable arguments using *args and added a sum calculation.

--- advanced.py
+++ advanced.py
@@ -1,4 +1,4 @@
-# Decorater
+# Decorater ..........
 # class Animal:
 #     @property # ye decorater obj ko call jiye bagheer bhi chla deta ha yani agar hum (obj.show() mian () nhi bhi lgay tu error hi ayga) iski example line numhber 8 pr ha.
 #     def show(self):
@@ -7,15 +7,29 @@
 # obj = Animal()
 # obj.show
 
-def decorate(func):
-    def wrapper(a,b): #if you don't know how many arguments you re there so you just need to replace (a,b) with (*args)
-        print("The additionof your numbers are")
-        func(a,b)
-        print("Thanks")
-    return wrapper
+# def decorate(func):
+#     def wrapper(a,b): #if you don't know how many arguments you re there so you just need to replace (a,b) with (*args)
+#         print("The additionof your numbers are")
+#         func(a,b)
+#         print("Thanks")
+#     return wrapper
 
-@decorate
-def addition(a,b):
-    print(f"Your total is {a + b}")
+# @decorate
+# def addition(a,b):
+#     print(f"Your total is {a + b}")
 
-addition(4,6)
+# addition(4,6)
+# ...
+
+# args and kwargs ............. args apke arguments ko tuple form main hty hain
+def addition(*args):
+    sum = 0
+    for i in args:
+        sum = sum + i    
+    print(sum)
+
+addition(2,3,4,5,6)
+
+# Kwargs ....................
+
+# ...
